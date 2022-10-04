1d69018e688267e588a25d201ff1e434aaa6f96d
wendrew oliveira <wendrew.soares@concil.com.br>
2022-10-04 00:18:12 -0300

    reordering folders

--- scripts/discard-dev-files.sh
+++ scripts/discard-dev-files.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -rf \{\{cookiecutter.project_slug\}\}/.git
-rm -rf \{\{cookiecutter.project_slug\}\}/backend/app/poetry.lock
+rm -rf \{\{cookiecutter.project_slug\}\}/app/poetry.lock
 git checkout \{\{cookiecutter.project_slug\}\}/README.md
 git checkout \{\{cookiecutter.project_slug\}\}/.gitlab-ci.yml
 git checkout \{\{cookiecutter.project_slug\}\}/cookiecutter-config-file.yml
